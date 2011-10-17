1d24bc7e1d716ba8a2be0bbeda4120b35661e003
Adam Faulconbridge <afaulconbridge@googlemail.com>
2011-10-17 11:33:59 +0100

    versin increment

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pyumpf", 
-    version="0.1.0", 
+    version="0.1.1", 
     author="Adam Faulconbridge", 
     author_email="afaulconbridge@googlemail.com", 
     packages = find_packages(),
